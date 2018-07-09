714eecef2b93c53cb17542e0e8b284be2888a31f
Conanap <albionapc@gmail.com>
2018-07-09 10:07:24 -0400

    debug print

--- A2/starter/opt.c
+++ A2/starter/opt.c
@@ -211,6 +211,8 @@ void opt_ref(pgtbl_entry_t *p) {
 	printf("\tin use %c, vaddr %d, num_to_ref %d\n", f->in_use, (int)f->vaddr, f->num_to_ref);
 
 	printf("ref: %d has num_to_ref %d on frame %d\n", (int) vaddr, f->num_to_ref, frame_num);
+	number* debug = tracker[get_hash(vaddr)]->item->num_fut_ref;
+	printf("ref: next num_to_ref: %d\n", debug->next->num_ref);
 	linked_list *ll = search_vaddr(vaddr);
 	if(ll == NULL) {
 		exit(1);
